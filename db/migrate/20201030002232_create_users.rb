5025f5f52db127f5e35ed6973a3c6157e6e1e6f6
Jmiller1294 <jayman1294@gmail.com>
2020-10-29 20:57:43 -0400

    added attributes to users migration table

--- db/migrate/20201030002232_create_users.rb
+++ db/migrate/20201030002232_create_users.rb
@@ -2,6 +2,10 @@ class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       t.string :name
+      t.string :occupation
+      t.integer :age
+      t.text :hobbies
+      t.string :hometown
 
       t.timestamps
     end
