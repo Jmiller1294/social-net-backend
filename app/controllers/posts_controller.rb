98fd84a424629da9d66906097181b665be906fad
Jmiller1294 <jayman1294@gmail.com>
2020-11-03 01:26:30 -0500

    added nested routes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,6 +10,7 @@ class PostsController < ApplicationController
     end
 
     def create
+    binding.pry
        post = Post.create(content: params[:content], date: params[:content])
        render json: post
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :friends
-  resources :posts
+  resources :users do
+    resources :friends
+    resources :posts
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
